1eff42fd524005d3fead91c3174470714f765e25
Liam Dawson <liamrdawson@gmail.com>
2024-06-18 20:23:51 +0100

    Set up inintial S3 Bucket.

--- packages/RemixConstruct/lib/constructs/RemixSite.ts
+++ packages/RemixConstruct/lib/constructs/RemixSite.ts
@@ -2,20 +2,17 @@ import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as cdk from "aws-cdk-lib";
 
+import { Bucket } from "./Bucket";
+
 type RemixSiteProps = {
   serverDir: string;
   publicDir: string;
 };
 
 export class RemixSite extends Construct {
-  constructor(scope: Construct, id: string, props: RemixSiteProps) {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id);
 
-    const remixBucket = new s3.Bucket(this, "StaticBucket", {
-      publicReadAccess: false,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-    });
+    new Bucket(this, "MyFirstBucket", { versioned: true });
   }
 }
