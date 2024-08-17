7a7ed49d3c990e5263203ffb92539ef73777e70c
Liam Dawson <liamrdawson@gmail.com>
2024-08-17 23:54:59 +0100

    cleanup

--- packages/constructs/RemixApp/server.ts
+++ packages/constructs/RemixApp/server.ts
@@ -1,10 +1,5 @@
 import { createRequestHandler } from "@remix-run/architect";
 import { ServerBuild } from "@remix-run/node";
-// import * as build from "@remix-run/dev/server-build";
-
-// if (process.env.NODE_ENV !== "production") {
-//   require("./mocks");
-// }
 
 const viteDevServer =
   process.env.NODE_ENV === "production"
@@ -14,12 +9,6 @@ const viteDevServer =
           server: { middlewareMode: true },
         })
       );
-
-// const remixHandler = createRequestHandler({
-//   build: viteDevServer
-//     ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
-//     : await import("./build/server/index.js"),
-// });
 
 export const handler = createRequestHandler({
   // @ts-expect-error server index.js doesn't always exist
