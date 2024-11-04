ab4a33188fc2a2ae0355a43cbcebf8adb1d363a1
Liam Dawson <liamrdawson@gmail.com>
2024-11-04 23:24:33 +0000

    grid content wrapper

--- packages/constructs/RemixApp/app/routes/_index.tsx
+++ packages/constructs/RemixApp/app/routes/_index.tsx
@@ -22,27 +22,28 @@ export default function IndexPage() {
   return (
     <main className="mt-layoutSection flex-1 text-dark">
       <Grid>
-        <section className="col-span-6 col-start-1 md:col-span-8 md:col-start-3 xl:col-span-6 xl:col-start-4">
-          <Heading level={1}>Welcome</Heading>
-          {/* Should be Text component variant of 'content' */}
-          <Text variant={"content"} className="mt-h1">
-            I&apos;m Liam Dawson. I build and optimise eCommerce websites for
-            consumer brands and write about my experiences here.
-          </Text>
-        </section>
-        <section className="mt-pageSection col-span-6 col-start-1 md:col-span-8 md:col-start-3 xl:col-start-4">
-          <Heading level={2}>Posts</Heading>
-          <ul className="mt-h2 list-disc space-y-4 pl-sm font-content text-body leading-body tracking-body">
-            {posts.map((post) => (
-              <li className="w-fit" key={post._id}>
-                {/* Should be TextLink with variants of 'content' and 'ui' */}
-                <ContentLink to={`/${post.slug.current}`}>
-                  {post.title}
-                </ContentLink>
-              </li>
-            ))}
-          </ul>
-        </section>
+        <div className="col-span-6 col-start-1 md:col-span-8 md:col-start-3 xl:col-span-6 xl:col-start-4">
+          <section>
+            <Heading level={1}>Welcome</Heading>
+            <Text variant={"content"} className="mt-h1">
+              I&apos;m Liam Dawson. I build and optimise eCommerce websites for
+              consumer brands and write about my experiences here.
+            </Text>
+          </section>
+          <section className="mt-pageSection">
+            <Heading level={2}>Posts</Heading>
+            <ul className="mt-h2 list-disc space-y-4 pl-sm font-content text-body leading-body tracking-body">
+              {posts.map((post) => (
+                <li className="w-fit" key={post._id}>
+                  {/* Should be TextLink with variants of 'content' and 'ui' */}
+                  <ContentLink to={`/${post.slug.current}`}>
+                    {post.title}
+                  </ContentLink>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
       </Grid>
     </main>
   );
