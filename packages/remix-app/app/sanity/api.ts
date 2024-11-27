26d849c3c19fa7d3cc1b3c0d3c8545035e0c8dad
Liam Dawson <liamrdawson@gmail.com>
2024-11-27 21:33:44 +0000

    api unit tests

--- packages/remix-app/app/sanity/api.ts
+++ packages/remix-app/app/sanity/api.ts
@@ -1,7 +1,14 @@
+import { Params } from "@remix-run/react";
+
 import { POST_QUERYResult } from "~/lib/cms/types";
 
 import { client } from "./client";
-import { POSTS_QUERY } from "./queries";
+import { POST_QUERY, POSTS_QUERY } from "./queries";
+
+export async function fetchPost(params: Params) {
+  const post: POST_QUERYResult = await client.fetch(POST_QUERY, params);
+  return post;
+}
 
 export async function fetchPosts() {
   const posts: POST_QUERYResult[] = await client.fetch(POSTS_QUERY);
--- packages/remix-app/app/sanity/queries.ts
+++ packages/remix-app/app/sanity/queries.ts
@@ -1,4 +1,12 @@
-export const POSTS_QUERY = `*[
+import { defineQuery } from "groq";
+
+export const POST_QUERY = defineQuery(
+  `*[_type == "post" && slug.current == $slug][0]`,
+);
+
+export const POSTS_QUERY = defineQuery(
+  `*[
     _type == "post"
     && defined(slug.current)
-  ]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`;
+  ]|order(publishedAt desc)[0...12]{_id, title, slug, publishedAt}`,
+);
