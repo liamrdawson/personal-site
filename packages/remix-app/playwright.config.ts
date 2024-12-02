b58870aad8d01b05c9112b462b336dc177eb9ba7
Liam Dawson <liamrdawson@gmail.com>
2024-12-02 22:20:26 +0000

    configure e2e test

---
+++ packages/remix-app/playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "@playwright/test";
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+
+export default defineConfig({
+  webServer: {
+    command: "pnpm build && pnpm dev",
+    port: 5173,
+    timeout: 60000,
+    reuseExistingServer: !process.env.CI,
+  },
+  testDir: "tests",
+});
