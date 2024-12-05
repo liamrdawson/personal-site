00ee132fac80c54109949f5700ec5a40f79e00e3
Liam Dawson <liamrdawson@gmail.com>
2024-12-05 22:08:02 +0000

    LRD-8 - remove Axebuilder

--- packages/remix-app/tests/blog.spec.ts
+++ packages/remix-app/tests/blog.spec.ts
@@ -1,4 +1,3 @@
-import AxeBuilder from "@axe-core/playwright";
 import { expect, test } from "@playwright/test";
 
 test.describe("blog post page", () => {
@@ -6,15 +5,6 @@ test.describe("blog post page", () => {
     await page.goto("/blog/scalable-remix-deployment-on-aws", {
       waitUntil: "networkidle",
     });
-
-    const content = await page.content();
-    console.log(content);
-    // check that there are no alt-text accessiblity violations
-    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-    const colorContrastViolations = accessibilityScanResults.violations.filter(
-      (v) => v.id === "area-alt",
-    );
-    expect(colorContrastViolations).toEqual([]);
 
     // check that all of the images have alt text
     const images = await page.locator("img").elementHandles();
