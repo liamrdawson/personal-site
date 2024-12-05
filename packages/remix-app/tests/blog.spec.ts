efcabac94ba1e1f2610dc837dd55c32d825ff1e2
Liam Dawson <liamrdawson@gmail.com>
2024-12-05 21:38:05 +0000

    LRD-8 - add alt text to images

---
+++ packages/remix-app/tests/blog.spec.ts
@@ -0,0 +1,22 @@
+import AxeBuilder from "@axe-core/playwright";
+import { expect, test } from "@playwright/test";
+
+test.describe("blog post page", () => {
+  test("images should have alt-text", async ({ page }) => {
+    await page.goto("/blog/scalable-remix-deployment-on-aws");
+
+    // check that there are no alt-text accessiblity violations
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    const colorContrastViolations = accessibilityScanResults.violations.filter(
+      (v) => v.id === "area-alt",
+    );
+    expect(colorContrastViolations).toEqual([]);
+
+    // check that all of the images have alt text
+    const images = await page.locator("img").elementHandles();
+    for (const image of images) {
+      const altText = await image.getAttribute("alt");
+      expect(altText).toBeTruthy();
+    }
+  });
+});
