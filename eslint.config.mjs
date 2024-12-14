2902893d3c53763b1d5a03a1bd3d6e4792b56b1a
Liam Dawson <liamrdawson@gmail.com>
2024-12-14 23:02:14 +0000

    LRD-3 - eslint ignore files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ export default [
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
-  { ignores: ["**/build", "**/dist"] },
+  { ignores: ["**/build", "**/dist", "**/.react-router"] },
   {
     name: "globals",
     languageOptions: { globals: { ...globals.browser, ...globals.node } },
