0aacd5f4c2a031cb058461b922b59c904fde8ee3
Liam Dawson <liamrdawson@gmail.com>
2026-02-20 23:46:45 +0000

    comment

--- packages/site/vitest.config.ts
+++ packages/site/vitest.config.ts
@@ -5,10 +5,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
    plugins: [
     devtoolsJson(),
-    // ...
   ],
   test: {
-    // ... Specify options here.
     include: ["**/__tests__/**"],
   },
 });
